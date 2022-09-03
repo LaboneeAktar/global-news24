8dc67f1ec7901a8a0097d77dd92365060ba066eb
laboneeaktar <labonipub@gmail.com>
2022-09-03 11:24:46 +0600

    author add

--- js/news.js
+++ js/news.js
@@ -34,21 +34,32 @@ const displayCategoryNews = categories => {
         const { title, details, thumbnail_url, author, total_view } = category;
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class ="card lg:card-side mb-10 shadow-xl">
+        <div class ="card lg:card-side mb-16 shadow-xl">
             <figure><img src="${thumbnail_url}" alt="Album"></figure>
             <div class="card-body">
                 <h2 class="card-title">${title}</h2>
                 <p>${details.length > 200 ? details.slice(0, 400) + '...' : details}</p>
-                    <div class="flex mt-10">
+                <div class="md:flex mt-10 space-x-28">
+                    <div class="flex">
                         <img src="${author.img}" class="h-14 w-14 rounded-full" alt="image">
                         <span class="ml-5">
-                        <h1>${author.name}</h1>
-                        <p>${author.published_date}</p>
+                            <h1 class="text-xl">${author.name}</h1>
+                            <p>${author.published_date}</p>
                         </span>
-                     </div>
-                     <p>Total View: ${total_view}K</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Details</button>
+                    </div> 
+                    <div>
+                        <p class="mt-3 text-xl"><i class="fa-regular fa-eye"></i> ${total_view}K</p>
+                    </div>
+                   <div class="mt-3">
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                   </div>
+                    <div class="card-actions md:justify-end">
+                        <button class="btn btn-secondary">Details <i class="fa-solid fa-arrow-right ml-3"></i></button>
+                    </div>
                 </div>
             </div>
         </div>
